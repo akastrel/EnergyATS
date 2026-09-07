0eaf617d9b6ea0c2d56cc063112605f06832a800
Artics <akastrel@gmail.com>
2026-09-07 10:04:10 +0300

    Release Energy ATS 0.3.11

--- energy_ats/app/main.py
+++ energy_ats/app/main.py
@@ -37,7 +37,7 @@ from power_transfer import PowerTransferController, TransferAction
 from state_store import StateStore
 
 
-APP_VERSION = "0.3.10"
+APP_VERSION = "0.3.11"
 
 
 DEFAULT_OPTIONS: dict[str, Any] = {
